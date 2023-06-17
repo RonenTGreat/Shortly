20ff310d86400dd7fa19d6b897b1da4732901cb1
RonenTGreat <ronenh53@gmail.com>
2023-06-17 00:37:43 +0000

    added customize colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      "cyan": "hsl(180, 66%, 49%)",
-      "darkViolet": "hsl(257, 27%, 26%)",
-      "myRed": "hsl(0, 87%, 67%)"
-    }
+    extend: {
+      colors: {
+        "cyan": "hsl(180, 66%, 49%)",
+        "darkViolet": "hsl(257, 27%, 26%)",
+        "myRed": "hsl(0, 87%, 67%)",
+        "veryDarkViolet": "hsl(260, 8%, 14%)",
+        "veryDarkBlue": "hsl(255, 11%, 22%)",
+      }
+    }, 
   },
   plugins: [],
 }
