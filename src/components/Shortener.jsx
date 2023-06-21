546a12c5b32de4165625aa7449ffe08cb693eaa0
RonenTGreat <ronenh53@gmail.com>
2023-06-21 17:27:28 +0000

    added states to display errors

--- src/components/Shortener.jsx
+++ src/components/Shortener.jsx
@@ -3,19 +3,22 @@ import bgMobile from "../assets/images/bg-shorten-mobile.svg";
 import bgDesktop from "../assets/images/bg-shorten-desktop.svg";
 
 const getLocalStorage = () => {
-    let links = localStorage.getItem("links")
+  let links = localStorage.getItem("links");
 
-    if(links){
-        return JSON.parse(localStorage.getItem("links"))
-    }else{
-        return []
-    }
-}
+  if (links) {
+    return JSON.parse(localStorage.getItem("links"));
+  } else {
+    return [];
+  }
+};
 
 const Shorten = () => {
   const [text, setText] = useState("");
   const [links, setLinks] = useState(getLocalStorage());
-  const [buttonText, setButtonText] = useState("Copy")
+  const [buttonText, setButtonText] = useState("Copy");
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isInvalidate, setIsInvalidate] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -23,7 +26,7 @@ const Shorten = () => {
       /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\\/\w.-]*)*\/?$/;
 
     if (!text) {
-      alert("Input is empty");
+      setIsEmpty(true);
     } else {
       if (urlRegex.test(text)) {
         const shortenLink = async () => {
@@ -34,23 +37,24 @@ const Shorten = () => {
 
           console.log(data.result);
           setLinks(data.result);
+          setIsSuccess(true);
           setText("");
         };
         shortenLink();
       } else {
-        alert("Invalid URL!");
+        setIsInvalidate(true);
       }
     }
   };
 
   const handleCopy = () => {
-    navigator.clipboard.writeText(links.full_short_link)
-    setButtonText("Copied")
-}
+    navigator.clipboard.writeText(links.full_short_link);
+    setButtonText("Copied");
+  };
 
-useEffect(() => {
-    localStorage.setItem("links", JSON.stringify(links))
-}, [links])
+  useEffect(() => {
+    localStorage.setItem("links", JSON.stringify(links));
+  }, [links]);
 
   return (
     <>
@@ -69,6 +73,15 @@ useEffect(() => {
               value={text}
               onChange={(e) => setText(e.target.value)}
             />
+            {isEmpty && (
+              <p className=" text-myRed text-sm lg:hidden">Please add a link</p>
+            )}
+            {isInvalidate && (
+              <p className=" text-myRed text-sm lg:hidden">
+                Invalidate link
+              </p>
+            )}
+
             <button
               className="btn-cta rounded-lg w-full md:w-40 md:ml-5"
               type="submit"
@@ -77,25 +90,46 @@ useEffect(() => {
               Shorten It!
             </button>
           </div>
+          {isEmpty && (
+            <p className=" text-myRed text-sm hidden lg:block">
+              Please add a link
+            </p>
+          )}
+          {isInvalidate && (
+            <p className=" text-myRed text-sm hidden lg:block">
+              Invalidate link
+            </p>
+          )}
         </form>
 
-        <div className="flex flex-col items-center justify-center bg-white
-        md:flex-row md:justify-between p-3 mt-3 rounded-lg shadow">
-          <article>
-            <h6 className=" mb-3 md:mb-0">{links.original_link}</h6>
-          </article>
-
-          <article>
-            <ul className="md:flex items-center">
-              <li className="md:mr-5">
-                <button className=" text-cyan">{links.full_short_link}</button>
-              </li>
-              <li>
-                <button onClick={handleCopy} className="btn-cta rounded-lg text-sm focus:bg-darkViolet">{buttonText}</button>
-              </li>
-            </ul>
-          </article>
-        </div>
+        {isSuccess && (
+          <div
+            className="flex flex-col lg:items-center justify-center bg-white
+        md:flex-row md:justify-between px-5 py-3 mt-3 rounded-lg shadow"
+          >
+            <article>
+              <h6 className=" mb-3 md:mb-0">{links.original_link}</h6>
+            </article>
+            <hr />
+            <article>
+              <ul className="md:flex items-center">
+                <li className="md:mr-5">
+                  <button className=" text-cyan">
+                    {links.full_short_link}
+                  </button>
+                </li>
+                <li>
+                  <button
+                    onClick={handleCopy}
+                    className="btn-cta rounded-lg text-sm focus:bg-darkViolet w-full"
+                  >
+                    {buttonText}
+                  </button>
+                </li>
+              </ul>
+            </article>
+          </div>
+        )}
       </section>
     </>
   );
